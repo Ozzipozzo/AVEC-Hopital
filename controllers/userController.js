256b7fa280441bf80f85b54b8d067629dc265605
Ozzipozzo <ghanmiabdallahb@gmail.com>
2021-09-15 13:36:29 +0200

    [BUG] User finding in DB while no user is registered

--- controllers/userController.js
+++ controllers/userController.js
@@ -4,10 +4,28 @@ const User = require('../models/UserModel');
 
 
 exports.userRegister = (req, res) => {
+
+    //retrieving user info
     const { login, password } = req.body;
-    User.create({ login, password}, (err, result) => {
+
+    //validate user input
+    if(!(login, password)) {
+        res.status(400).send('All input is required');
+    }
+
+    //check if user already exist
+    const oldUser = User.findOne({ login })
+    console.log(oldUser);
+    if (oldUser) {
+        return res.status(409).send('Login already exist, please choose another one');
+    }
+
+    //Encrypt password
+    encryptedPassword = bcrypt.hash(password, 10);
+    
+    User.create({ login: login.toLowerCase(), password: encryptedPassword}, (err, result) => {
         if (err) throw new Error(err);
-        console.log(result);
+        res.status(200).send('Vous êtes enregistré');
     })
 }
 
